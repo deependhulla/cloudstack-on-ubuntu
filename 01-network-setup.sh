49151a25b7a2d9771d6876b040bba49c52320534
Deepen Dhulla <deepen@deependhulla.com>
2024-12-23 11:27:53 +0000

    update on Mon Dec 23 11:27:53 AM UTC 2024

--- 01-network-setup.sh
+++ 01-network-setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-apt-get -y  install bridge-utils openvswitch-switch ethtool net-tools
 
-
-rm -f /etc/netplan/50-cloud-init.yaml
-
-DNS=8.8.8.8
 hostipandsub=192.168.30.123/24
 gateway=192.168.30.242
 interface=enp1s0
+DNS=8.8.8.8
+
+apt-get -y  install bridge-utils openvswitch-switch ethtool net-tools
+
+echo >  /etc/netplan/50-cloud-init.yaml
+echo "network: {config: disabled}" > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
+chmod 600  /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
 
 
   cat << EOF > /etc/netplan/01-netcfg.yaml
@@ -38,4 +40,7 @@ EOF
 
 
 chmod 600  /etc/netplan/01-netcfg.yaml 
+
+netplan generate
+netplan apply
 
