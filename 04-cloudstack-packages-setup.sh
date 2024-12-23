bdc3e5e5739629d954a12d7af5005085b1f69e4a
Deepen Dhulla <deepen@deependhulla.com>
2024-12-23 11:50:12 +0000

    update on Mon Dec 23 11:50:12 AM UTC 2024

--- 04-cloudstack-packages-setup.sh
+++ 04-cloudstack-packages-setup.sh
@@ -7,5 +7,9 @@ mkdir -p /etc/apt/keyrings
 wget -O- https://download.cloudstack.org/release.asc 2>/dev/null | gpg --dearmor | sudo tee /etc/apt/keyrings/cloudstack.gpg > /dev/null
 echo deb [signed-by=/etc/apt/keyrings/cloudstack.gpg] https://download.cloudstack.org/ubuntu $U_VERSION $CS_VERSION > /etc/apt/sources.list.d/cloudstack.list
 
+## cli tool
+wget -q https://github.com/apache/cloudstack-cloudmonkey/releases/download/6.4.0/cmk.linux.x86-64 -O /usr/bin/cmk > /dev/null
+chmod +x /usr/bin/cmk
+
 apt-get update
 
